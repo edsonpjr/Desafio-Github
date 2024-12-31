8b98a68ebb8568e27b5c9150f324323ef9cea42f
Edson Júnior <89272697+edsonpjr@users.noreply.github.com>
2024-12-31 18:54:55 +0000

    Implementing palidorme project

---
+++ palindromos/palin.py
@@ -0,0 +1,11 @@
+# Checar se uma palavra é um palíndromo
+
+palavra = input('Digite uma palavra: ')
+
+# Invertendo a palavra
+palavra_invertida = palavra[::-1]
+
+if palavra == palavra_invertida:
+    print('A palavra é um palíndromo!')
+else:
+    print('A palavra não é um palíndromo!')
